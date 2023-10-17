11a092670d3f8a787efc63b5e81c6e96f49fc555
ayeshasikander <sikanderayesha46@gmail.com>
2023-10-17 15:38:34 +0500

    add collection part

--- script.js
+++ script.js
@@ -33,3 +33,34 @@ gsap.to("#main", {
     }
 })
 
+var tl = gsap.timeline({ repeat: -1 });
+
+tl
+    .to(".img-section", {
+        width: "100%",
+        ease: Expo.easeInOut,
+        stagger: 2
+
+    }, 'a')
+    .to(".text h1", {
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: 0
+    }, 'a')
+    .to(".text h1", {
+        delay: 2,
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: "-100%"
+    }, 'a')
+    .to("#paragraph p", {
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: 0
+    }, 'a')
+    .to("#paragraph p", {
+        delay: 2,
+        ease: Expo.easeInOut,
+        stagger: 2,
+        top: "-100%"
+    }, 'a')
